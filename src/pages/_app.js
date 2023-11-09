0788c9b2eb41bcc7f1ccca516cf483d6e6b975dd
mhdfrhan <mhdfarhan5148@gmail.com>
2023-11-09 16:48:31 +0700

    Beberapa perbaikan

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,14 +2,14 @@
 
 import Layout from '@/components/Layout'
 import '@/styles/globals.css'
-import AOS from "aos";
+import Aos from "aos";
 import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
 	useEffect(() => {
 		import('preline')
 
-		AOS.init({
+		Aos.init({
       easing: "ease-out-cubic",
       once: true,
       offset: 50,
